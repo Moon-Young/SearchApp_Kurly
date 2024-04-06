cf93d70411a27560cd84c4c1ca085e9a03b97f70
Moon-Young <oooo5299@gmail.com>
2024-04-07 04:32:22 +0900

    Initial Setup

---
+++ SearchApp_Kurly/Presentation/Views/SearchViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  SearchApp_Kurly
+//
+//  Created by Moonyoung Kim on 4/7/24.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindViewModel()
+        setupLayout()
+        setupViews()
+        setupTableView()
+        
+    }
+
+    
+    
+    
+    func bindViewModel() {
+        
+    }
+    func setupLayout() {
+
+    }
+    func setupViews() {
+        self.view.backgroundColor = .white
+    }
+    func setupTableView() {
+        
+    }
+    
+
+}
+
+
