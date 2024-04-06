635439baa2bb985504dba516a8d67f3c3231d1f7
Moon-Young <oooo5299@gmail.com>
2024-04-07 03:47:15 +0900

    Initial Commit

---
+++ SearchApp_Kurly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchApp_Kurly
+//
+//  Created by Moonyoung Kim on 4/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SearchApp_KurlyUITests/SearchApp_KurlyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SearchApp_KurlyUITestsLaunchTests.swift
+//  SearchApp_KurlyUITests
+//
+//  Created by Moonyoung Kim on 4/7/24.
+//
+
+import XCTest
+
+final class SearchApp_KurlyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
